46ca583c83d07dbafcd9b2ab5ea78c512d96d5ef
Keita Akutsu <kakts.git@gmail.com>
2018-07-22 23:15:13 +0900

    Add chapter2-3-merge-sort.js and chapter4-1-maximum-subarray.js

--- chapter2/2-3-merge-sort.js
+++ chapter2/2-3-merge-sort.js
@@ -1,5 +1,6 @@
 
 function merge(list, p, q, r) {
+  console.log(list, p, q, r)
   // 分割する配列の左右それぞれの要素数取得
   const n1 = q - p;
   const n2 = r - q;
@@ -8,20 +9,15 @@ function merge(list, p, q, r) {
   const R = [];
   // L[1...n1 + 1] と R[1..n2+1]を２つの新しい配列とする
   for (let i = 0; i < n1; i++) {
-    console.log(p + i)
     L[i] = list[p + i];
   }
-  console.log(L);
 
   for (let j = 0; j <= n2; j++) {
     R[j] = list[q + j];
   }
-  console.log(R);
   // sentinelとして、終了判定に使う値をいれる。 ここではInfinityをいれる
   L[n1] = Infinity;
   R[n2 + 1] = Infinity;
-  console.log(L, R)
-  console.log(L, R)
   let i = j = 0;
   for (let k = p; k < r; k++) {
     if (L[i] <= R[j]) {
@@ -31,15 +27,25 @@ function merge(list, p, q, r) {
       list[k] = R[j];
       j += 1;
     }
-    console.log(list);
+    // console.log(list);
   }
+  console.log('before:', list);
   return list
 }
 
-
-// function mergeSort(A, p, r) {
-//   if (p < r) {
-//     q = 
-//   }
-// }
-console.log(merge([2, 4, 5, 7, 1, 2, 3, 6], 0, 4, 7));
+function mergeSort(A, p, r) {
+  console.log(p, r)
+  if (p < r) {
+    let q =  Math.floor((p + r) / 2);
+    console.log('q is ', q);
+    mergeSort(A, p, q); // 左半分
+    console.log('-----', A)
+    mergeSort(A, q + 1, r); // 右半分
+    // これにより 左、右それぞれはソートされている
+    
+    return merge(A, p, q, r);
+  } else {
+    // do nothing
+  }
+}
+console.log(mergeSort([2, 7, 5, 4, 4, 1, 2, 6], 0, 7));
---
+++ chapter4/4-1-maximum-subarray.js
@@ -0,0 +1,74 @@
+// 4.1 最大部分配列問題
+
+// 与えられた配列の中から中間点(mid)をまたいだ最大部分配列をもとめる
+function findMaxCrossingSubArray(list, low , mid, high) {
+
+  // 左半分の最大部分配列を決定する
+  let leftSum = -Infinity;  // これまでに発見した最大の和
+  let sum = 0; 
+  let maxLeft = 0;
+
+  // midから開始し、lowまで減らす
+  // したがって 調べられる任意の部分配列の形は list[i...mid]
+  for (let i = mid; i >= low; i--) {
+    sum += list[i];
+    if (sum > leftSum) {
+      leftSum = sum;
+      maxLeft = i;
+    }
+  }
+
+  // 右半分の最大部分配列を決定する
+  let rightSum = -Infinity;
+  sum = 0;
+  let maxRight = 0;
+  for (j = mid + 1; j < high; j++) {
+    sum += list[j];
+    if (sum > rightSum) {
+      rightSum = sum;
+      maxRight = j;
+    }
+  }
+
+  // 左右それぞれの最大部分配列のインデックスと 最大部分配列の和
+  return {
+    low: maxLeft,
+    high: maxRight,
+    sum: leftSum + rightSum
+  };
+}
+
+// 
+function findMaximumSubArray(list, low, high) {
+  if (high == low) {
+    return {
+      low,
+      high,
+      sum: list[low]
+    }; // 基底: 要素数1
+  }
+
+  // 中間値のインデックス
+  let mid = Math.floor((low + high) / 2);
+
+  const leftMaximum = findMaximumSubArray(list, low, mid);
+
+  const rightMaximum = findMaximumSubArray(list, mid + 1, high);
+
+  // 中間値をまたいだ最大部分配列
+  const crossingMaximum = findMaxCrossingSubArray(list, low, mid, high);
+
+  if (leftMaximum.sum >= rightMaximum.sum && leftMaximum.sum >= crossingMaximum.sum) {
+    return leftMaximum;
+  } else if (rightMaximum.sum >= leftMaximum.sum && rightMaximum.sum >= crossingMaximum.sum) {
+    return rightMaximum;
+  } else {
+    return crossingMaximum;
+  }
+}
+
+
+const list = [1, 4, 2, -500, 5, 3, 2, 100];
+console.log(findMaximumSubArray(list, 0, list.length))
+
+
